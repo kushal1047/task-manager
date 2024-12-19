6aad73fff6f25803d2a09fced4357bf50f3e6308
Kushal Niraula <kushal463334@gmail.com>
2024-12-19 10:30:00 +1000

    Connect to database

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// server/server.js
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
