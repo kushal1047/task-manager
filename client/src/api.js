c374686aba8d4dad9d8c12820240c4c82e790dcc
Kushal Niraula <kushal463334@gmail.com>
2024-12-30 11:42:00 +1000

    Create axios functions and export them for components use

---
+++ client/src/api.js
@@ -0,0 +1,8 @@
+// client/src/api.js
+import axios from "axios";
+
+export const fetchTasks = () => axios.get("/api/tasks");
+export const createTask = (title) => axios.post("/api/tasks", { title });
+export const updateTask = (id, completed) =>
+  axios.put(`/api/tasks/${id}`, { completed });
+export const deleteTask = (id) => axios.delete(`/api/tasks/${id}`);
