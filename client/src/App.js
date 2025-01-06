daa537d6348315219b7cee9c7ce6e2dc8a895ee7
Kushal Niraula <kushal463334@gmail.com>
2025-01-06 11:03:00 +1000

    Create functions to load tasks and create new task

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { fetchTasks, createTask, updateTask, deleteTask } from './api';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  const [tasks, setTasks] = useState([]);
 
-export default App;
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  const loadTasks = async () => {
+    const res = await fetchTasks();
+    setTasks(res.data);
+  };
+
+  const handleAdd = async title => {
+    const res = await createTask(title);
+    setTasks([res.data, ...tasks]);
+  };
