0466f84b00f0657c3d5b4a344c2274d97cedbcd2
Kushal Niraula <kushal463334@gmail.com>
2025-01-06 11:18:00 +1000

    Update App.js to render all components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { fetchTasks, createTask, updateTask, deleteTask } from './api';
-import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
+import React, { useEffect, useState } from "react";
+import { fetchTasks, createTask, updateTask, deleteTask } from "./api";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
@@ -15,16 +15,24 @@ export default function App() {
     setTasks(res.data);
   };
 
-  const handleAdd = async title => {
+  const handleAdd = async (title) => {
     const res = await createTask(title);
     setTasks([res.data, ...tasks]);
   };
   const handleToggle = async (id, completed) => {
     const res = await updateTask(id, completed);
-    setTasks(tasks.map(t => (t._id === id ? res.data : t)));
+    setTasks(tasks.map((t) => (t._id === id ? res.data : t)));
   };
 
-  const handleDelete = async id => {
+  const handleDelete = async (id) => {
     await deleteTask(id);
-    setTasks(tasks.filter(t => t._id !== id));
+    setTasks(tasks.filter((t) => t._id !== id));
   };
+  return (
+    <div className="container">
+      <h1>Task Manager</h1>
+      <TaskForm onAdd={handleAdd} />
+      <TaskList tasks={tasks} onToggle={handleToggle} onDelete={handleDelete} />
+    </div>
+  );
+}
