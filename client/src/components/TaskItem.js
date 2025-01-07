179eb473ff2289a7b1dc737d4b642c60ecd6d557
Kushal Niraula <kushal463334@gmail.com>
2025-01-07 11:33:00 +1000

    Modify css for better look and design

--- client/src/components/TaskItem.js
+++ client/src/components/TaskItem.js
@@ -8,10 +8,17 @@ export default function TaskItem({ task, onToggle, onDelete }) {
         checked={task.completed}
         onChange={() => onToggle(task._id, !task.completed)}
       />
-      <span style={{ textDecoration: task.completed ? "line-through" : "" }}>
-        {task.title}
+      <span
+        style={{
+          textDecoration: task.completed ? "line-through" : "",
+          marginLeft: "1rem",
+        }}
+      >
+        <h4> {task.title}</h4>
       </span>
-      <button onClick={() => onDelete(task._id)}>Delete</button>
+      <button className="delete-link" onClick={() => onDelete(task._id)}>
+        Delete
+      </button>
     </li>
   );
 }
