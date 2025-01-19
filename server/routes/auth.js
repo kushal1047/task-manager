188e3f688a842d3c92ea5663a80ff9c5dc5b583c
Kushal Niraula <kushal463334@gmail.com>
2025-01-19 12:47:00 +1000

    Generate a token using userId for each successful login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -37,7 +37,10 @@ router.post("/login", async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-    res.json({ user: { id: user._id, username } });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token, user: { id: user._id, username } });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
