eba9cda1509fa7968920ca6064798419cacbd07c
Kushal Niraula <kushal463334@gmail.com>
2025-01-19 12:02:00 +1000

    Generate a token using userId for each registeration

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,12 +8,21 @@ const router = express.Router();
 // Register
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const exists = await User.findOne({ username });
-  if (exists) return res.status(400).json({ msg: "Username taken" });
+  try {
+    const exists = await User.findOne({ username });
+    if (exists) return res.status(400).json({ msg: "Username taken" });
 
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-  const user = new User({ username, password: hash });
-  await user.save();
+    const user = new User({ username, password: hash });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(201).json({ token, user: { id: user._id, username } });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
