b79b197a4e1f4090195920ca1da9125b109882a7
Kushal Niraula <kushal463334@gmail.com>
2025-01-19 13:08:00 +1000

    Create an auth function as middleware to verify token

---
+++ server/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token) return res.status(401).json({ msg: "No token, auth denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ msg: "Token invalid" });
+  }
+}
+
+module.exports = auth;
