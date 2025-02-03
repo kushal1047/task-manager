24586b589e3d1e1acf9a36ea511125856306d79e
Kushal Niraula <kushal463334@gmail.com>
2025-02-03 16:10:00 +1000

    Modify Task model to include virtual user property

--- server/models/Task.js
+++ server/models/Task.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId, // reference to User
+      ref: "User",
+      required: true,
+    },
     title: { type: String, required: true },
     completed: { type: Boolean, default: false },
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
+TaskSchema.index({ user: 1, createdAt: -1 }); // index for faster lookups by user
 module.exports = mongoose.model("Task", TaskSchema);
