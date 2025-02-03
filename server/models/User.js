61090f1fbe1fcd829d5bbf52871075b4b0e07207
Kushal Niraula <kushal463334@gmail.com>
2025-02-03 16:28:00 +1000

    Modify User model to include virtual tasks property

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,15 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
+
+// virtual to populate all tasks for a user
+UserSchema.virtual("tasks", {
+  ref: "Task", // refers to Task model
+  localField: "_id", // matches this User’s _id
+  foreignField: "user", // to Task.user
+  justOne: false,
+});
 
 module.exports = mongoose.model("User", UserSchema);
